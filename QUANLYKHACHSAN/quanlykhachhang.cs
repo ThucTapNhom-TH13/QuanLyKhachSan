90ff7156d23d9d83ef0691279958c60aa28a0ad3
phuonglinhth238 <phuonglinhth238@gmail.com>
2017-05-29 17:17:43 +0700

    QuanLyKH

--- QUANLYKHACHSAN/quanlykhachhang.cs
+++ QUANLYKHACHSAN/quanlykhachhang.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.SqlClient;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -12,9 +13,25 @@ namespace QUANLYKHACHSAN
 {
     public partial class quanlykhachhang : Form
     {
+        public string strConnection = @"Data Source=LINH\SQLEXPRESS;Initial Catalog=QUANLYKHACHSAN1;Integrated Security=True";
+        public SqlConnection conn = null;
+        string Gender = "";
         public quanlykhachhang()
         {
             InitializeComponent();
+        }
+        private void loadData()
+        {
+            SqlDataAdapter da = new SqlDataAdapter("SELECT* FROM KHACHHANG", conn);
+            DataTable dt = new DataTable();
+            da.Fill(dt);
+            dataGridView1.DataSource = dt;
+        }
+        private void quanlykhachhang_Load(object sender, EventArgs e)
+        {
+            conn = new SqlConnection(strConnection);
+            conn.Open();
+            loadData();
         }
     }
 }
