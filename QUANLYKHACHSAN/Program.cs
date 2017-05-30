ec1fb90c8e6b5e5cd794633aa9470fbbf80c6bde
luffy-99 <lehuyitmta@gmail.com>
2017-05-30 07:10:07 +0700

    updateNhanVien

--- QUANLYKHACHSAN/Program.cs
+++ QUANLYKHACHSAN/Program.cs
@@ -16,7 +16,7 @@ namespace QUANLYKHACHSAN
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new quanlykhachhang());
+            Application.Run(new quanlynv());
         }
     }
 }
